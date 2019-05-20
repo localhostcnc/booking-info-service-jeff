4ce99e29ad7a606ac82c48b5396201aac2a31d8b
Jeff Day <jeffrey.day00@gmail.com>
2019-05-20 15:17:56 -0700

    Sorry for the messy git workflow issues. Trying to figure it all out.

--- test/apiTest.js
+++ test/apiTest.js
@@ -1,12 +1,13 @@
+/* eslint-disable no-undef */
 const request = require('supertest');
 const app = require('../server/index.js');
 
 describe('GET /listings', () => {
   it('respond with a list of all listings', (done) => {
     request(app)
-    .get('/listings')
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(200, done);
+      .get('/listings')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
   });
 });
